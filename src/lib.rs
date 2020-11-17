8226265a07cf2cef23ed256a6254b36d3a333e22
Dan Bruder <danb@structionsite.com>
2020-11-17 12:33:27 -0500

    what if

--- src/lib.rs
+++ src/lib.rs
@@ -24,16 +24,16 @@ mod domain {
 
     #[derive(Clone)]
     pub struct Entity(Value);
+
     #[derive(Clone)]
     pub struct Selection(pub Vec<Value>);
+
     #[derive(Clone)]
     pub struct Payload(pub Vec<Entity>);
 
     #[derive(Clone)]
-    pub struct MapReduce(Vec<Entity>);
-
-    #[derive(Clone)]
     pub struct Query(pub Box<Filter>, pub Selection);
+
     #[derive(Clone)]
     pub struct ExecutionResult(pub Query, pub Payload);
 
@@ -69,6 +69,48 @@ pub fn main() {
     let filter = Filter::by_owner_id("dan".into()).and(Filter::by_type("user".into()));
     let selection = Selection(vec![Value::String("owner_id".to_string())]);
 
-    let query = Query(filter, selection);
+    struct User {
+        first_name: String,
+        last_name: String,
+    }
+    struct Bot {
+        full_name: String,
+    }
+
+    let user_map_fn = |first_name: String, last_name: String| -> User {
+        User {
+            first_name,
+            last_name,
+        }
+    };
+
+    let bot_map_fn = |full_name: String| -> Bot { Bot { full_name } };
+    let both_map_fn = |users: Vec<User>, bots: Vec<Bot>| -> Vec<(User, Bot)> { vec![] };
+
+    // This just feels like a bad idea all around.
+    let query = query! {
+        sub_query! {
+            filter {
+                by_owner_id("dan"),
+                by_type("user"),
+            }
+            select {
+                first_name: String
+                last_name: String
+            }
+            map(map_fn)
+        },
+        sub_query! {
+            filter {
+                by_type("bot"),
+            }
+            select {
+                full_name: String
+            }
+            map(bot_map_fn)
+        },
+        map(both_map_fn)
+    };
+
     let _ = infra::run(query);
 }
